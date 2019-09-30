f8368ec7abc4e600a2f24e1a5beb55c5922fbb68
Russ <rscharf33@gmail.com>
2019-09-30 00:32:23 -0500

    fda

---
+++ src/binary_search.java
@@ -0,0 +1,29 @@
+
+public class binary_search {
+
+	public static void main(String[] args) {
+		int[] arr = {2, 3, 4, 10, 40}; 
+		int find=3; 
+		System.out.println(search(arr, 0, arr.length-1, find));
+
+	}
+	
+	public static int search(int arr[], int low, int high, int find) {
+		if(high<low) return -1; 
+		
+		int mid = (low+high)/2; 
+			
+		if(arr[mid]==find) {
+			return mid; 
+		}
+		else if (arr[mid]<find){
+			return search(arr, mid+1, high, find); 
+		}
+		else {
+			return search(arr, low, mid-1, find);
+		}
+
+
+	}
+
+}
---
+++ src/hashset.java
@@ -0,0 +1,31 @@
+// HashSet methods: add, remove, contains, size, isEmpty
+
+import java.util.*; 
+
+public class hashset {
+
+	public static void main(String[] args) {
+		// initialize set
+		HashSet<String> countries = new HashSet<>(); 
+		
+		// add elements
+		countries.add("India"); 
+		countries.add("Australia"); 
+		countries.add("South Africa"); 
+		countries.add("India"); 
+		
+		System.out.println(countries);
+		
+		countries.remove("Australia"); 
+		
+		System.out.println(countries);
+		
+		System.out.println("Contains India? " + countries.contains("India"));
+		
+		System.out.println("Size: " + countries.size());
+		
+		System.out.println("Is empty? " + countries.isEmpty());
+
+	}
+
+}
