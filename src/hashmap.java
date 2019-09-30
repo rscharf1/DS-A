5124779a346f319aca523a49053cd2a4c0e6cdd4
Russ <rscharf33@gmail.com>
2019-09-30 00:05:47 -0500

    fa

---
+++ src/hashmap.java
@@ -0,0 +1,30 @@
+// HashMap methods: put, get, remove, size, isEmpty
+import java.util.*;
+
+public class hashmap {
+
+	public static void main(String[] args) {
+		// initialize map
+		HashMap<String, String> cities = new HashMap<>();
+		
+		// add items
+		cities.put("England", "London"); 
+		cities.put("Germany", "Berlin"); 
+		cities.put("Norway", "Oslo"); 
+		cities.put("USA", "Washington DC"); 
+		
+		System.out.println(cities);
+		
+		System.out.println(cities.get("England"));
+		
+		cities.remove("England");
+		
+		System.out.println(cities);
+		
+		System.out.println("Size: " + cities.size());
+		
+		System.out.println("Is empty? " + cities.isEmpty());
+
+	}
+
+}
