8a7d9f3a5b3201bce39ac3a64dbea0c944670b0c
Russ <rscharf33@gmail.com>
2019-10-08 21:50:56 -0500

    fdsa

--- src/test.java
+++ src/test.java
@@ -1,8 +1,27 @@
-
 public class test {
 
 	public static void main(String[] args) {
+		int size = 2; 
+		int base = size/2*3+8*size; 
+		int height = (size/2+1)+4*size; 
+		System.out.println("Base: " + base); 
+		System.out.println("Height: " + height); 
 		
+		for(int i=0; i<height; i++) {
+			int stars = 2*i+1; 
+			int spacesBefore = (base-stars)/2;
+
+			for(int x=0; x<spacesBefore; x++) {
+				System.out.print(" ");
+			}
+			for(int x=0; x<stars; x++) {
+				System.out.print("*");
+			}
+			for(int x=0; x<spacesBefore; x++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 
 	}
 
---
+++ src/treeset.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+
+public class treeset {
+
+	public static void main(String[] args) {
+		// initialize treeset
+		TreeSet<String> ts = new TreeSet<>(); 
+		
+		// add elements
+		ts.add("A"); 
+		ts.add("C"); 
+		ts.add("B"); 
+		
+		System.out.println(ts);
+		
+
+	}
+
+}
