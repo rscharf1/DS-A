b3897512e31555b16c88d9f9646945548d50941d
Russ <rscharf33@gmail.com>
2019-10-31 11:00:36 -0500

    fds

---
+++ src/MyClass.java
@@ -0,0 +1,19 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		Student mark = new Student(); // mark -> object or instance of class Student 
+		
+		mark.id = 1; 
+		mark.name = "Mark";
+		mark.age = 15; 
+		
+		Student test = new Student();
+		
+		System.out.println(mark.name + " is " + mark.age);
+		
+		System.out.println(test.age);
+		
+	}
+
+}
---
+++ src/Student.java
@@ -0,0 +1,13 @@
+
+public class Student {
+	int id; 
+	String name; 
+	int age; 
+	
+	Student() {
+		id=0; 
+		name="";
+		age=0; 
+	}
+
+}
